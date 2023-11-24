e3149e9e831fc533a0ea914d3fe5a0d85d26ed73
nxx5xxx <dnwnx123@naver.com>
2023-11-25 00:31:06 +0900

    질문답변

---
+++ test/src/test2/Printer.java
@@ -0,0 +1,5 @@
+package test2;
+
+public class Printer {
+	public static double PI = 3.141592;
+}
---
+++ test/src/test2/staticTest.java
@@ -0,0 +1,10 @@
+package test2;
+
+public class staticTest {
+
+	public static void main(String[] args) {
+		double circleWidth = Math.pow(5, 2) * Printer.PI;
+		System.out.println(circleWidth);
+	}
+
+}
