6877cd675fac2db19ee902d6015f46eabd11eed2
nxx5xxx <dnwnx123@naver.com>
2023-12-05 00:21:21 +0900

    add test

---
+++ test/src/test2/SwitchTest.java
@@ -0,0 +1,16 @@
+package test2;
+
+public class SwitchTest {
+
+	public static void main(String[] args) {
+		long a = 10L;
+		long b = 10;
+		char c = 'c';
+		switch(c) {
+		case 'c' : 
+			break;
+		}
+// JDK 1.8 버전 long 지원안됨
+	}
+
+}
---
+++ test/src/test2/Unicode.java
@@ -0,0 +1,13 @@
+package test2;
+
+public class Unicode {
+
+	public static void main(String[] args) {
+		String a = "こんにちは";
+		System.out.println(a);
+		System.out.println(a.codePointAt(0));
+		System.out.println((char)12371);
+
+	}
+
+}
